e4a1f12ebcae7e6a3fc0ac25b937181d4697aa47
Gimmy <gamalgiali@gmail.com>
2020-05-09 22:47:36 -0400

    task: html parser and load time

--- scrapers/scraper.py
+++ scrapers/scraper.py
@@ -14,8 +14,7 @@ def get_driver():
     return driver
 
 def connect_to_base(browser, page_number=1):
-    # url = 'https://www.sec.gov/Archives/edgar/data/1341439/000119312518090656/0001193125-18-090656.txt'
-    import pdb; pdb.set_trace()
+    # base_url = "https://www.sec.gov/Archives/edgar/data/1341439/000119312518090656/0001193125-18-090656.txt"
     base_url = f"https://news.ycombinator.com/news?p={page_number}"
     connection_attempts = 0
 
@@ -24,8 +23,8 @@ def connect_to_base(browser, page_number=1):
             browser.get(base_url)
             # wait for table element with id = "hnmain" to load 
             # before returning True
-            wait = WebDriverWait(browser, 5)
-            wait.until(EC.presence_of_element_located((By.ID, "hnmain")))
+            # wait = WebDriverWait(browser, 5)
+            # wait.until(EC.presence_of_element_located((By., "hnmain")))
             return True
         except Exception as ex:
             connection_attempts += 1
@@ -33,6 +32,45 @@ def connect_to_base(browser, page_number=1):
             print(f"Connection attempt: {connection_attempts}")
     return False
 
-# def parse_html(html):
-#     # create soup object
-#    soup = BeautifulSoup(html, "html.parser")
+def parse_html(html):
+    soup = BeautifulSoup(html, "html.parser")
+    output_list = []
+    # parse soup object to get article id, rank, score, and title
+    tr_blocks = soup.find_all("tr", class_="athing")
+    article = 0
+    for tr in tr_blocks:
+        article_id = tr.get("id")
+        article_url = tr.find_all("a")[1]["href"]
+        # check if article is a hacker news article
+        if "item?id=" in article_url:
+            article_url = f"https://news.ycombinator.com/{article_url}"
+        load_time = get_load_time(article_url)
+
+        try:
+            score = soup.find(id=f'score_{article_id}').string
+        except Exception as ex:
+            score = '0 points'
+        
+        article_info = {
+            'id': article_id,
+            'load_time': load_time,
+            'rank': tr.span.string,
+            'score': score,
+            'title': tr.find(class_='storylink').string,
+            'url': article_url
+        }
+
+def get_load_time(url):
+    try:
+        # set headers
+        headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+        # make get request to article_url
+        response = requests.get(
+            article_url, headers=headers, stream=True, timeout=3.000)
+        # get page load time
+        load_time = response.elapsed.total_seconds()
+    except Exception as ex:
+        load_time = 'Loading Error'
+    return load_time
+
+
